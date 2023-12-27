bf34fccc258bac7b1fa628af24bb0ae71729b019
Anggraini Andari <anggrainiandari@gmail.com>
2023-12-27 15:26:31 +0700

    portofolio done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        // Toggle nav list dan burger class
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+});
